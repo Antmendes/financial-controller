6d35a39cf85e1d92b07475228de95293454b5f65
Antmendes <antoniomendesjunior02@gmail.com>
2023-06-22 11:01:37 -0300

    estilazando os botoes criados para funcionar como radio

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
 
@@ -54,11 +54,26 @@ display: grid;
 grid-template-columns: 1fr 1fr;
 gap: 0.5rem;
 
-button {
-    height: 4rem;
+`
+
+interface RadioboxProps {   // interface criada para poder passar a propriedade isActive
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
+}
+
+export const RadioBox = styled.button<RadioboxProps>`
+
+height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: transparent;
+
+    background: ${(props) => props.isActive ? transparentize(0.9,colors[props.activeColor]) : 'transparent'};  //função para mudar a cor do botão.
+                                             //funçaõ do polished pra transparecer a cor
     display: flex;
     align-items: center;
     justify-content: center;
@@ -80,5 +95,4 @@ button {
         font-size: 1rem;
         color: var(--text-title);
     }
-}
 `
